c6f6fe34c4309e8779506e1ce21a14984921d8c7
Afaque783 <ahmed.afaque783@gmail.com>
2024-01-14 15:24:20 +0530

    Filtering Jobs: Radio Group Component (Filtering Jobs by Category and used Radio Component for less code)

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -21,38 +21,21 @@
             <x-text-input name="max_salary" value="{{ request('max_salary') }}" placeholder="To" />
           </div>
         </div>
-
-        <div>3</div>
         
         <div>
           <div class="mb-1 font-semibold">Experience</div>
 
-          <label for="experience" class="mb-1 flex items-center">
-            <input type="radio" name="experience" value=""
-              @checked(!request('experience')) />
-            <span class="ml-2">All</span>
-          </label>
+          <x-radio-group name="experience"
+            :options="\App\Models\Job::$experience" />
 
-          <label for="experience" class="mb-1 flex items-center">
-            <input type="radio" name="experience" value="entry"
-              @checked('entry' === request('experience')) />
-            <span class="ml-2">Entry</span>
-          </label>
+        </div>
+        <div>
+          <div class="mb-1 font-semibold">Category</div>
 
-          <label for="experience" class="mb-1 flex items-center">
-            <input type="radio" name="experience" value="intermediate"
-              @checked('intermediate' === request('experience')) />
-            <span class="ml-2">Intermediate</span>
-          </label>
-
-          <label for="experience" class="mb-1 flex items-center">
-            <input type="radio" name="experience" value="senior"
-              @checked('senior' === request('experience')) />
-            <span class="ml-2">Senior</span>
-          </label>
+          <x-radio-group name="category"
+            :options="\App\Models\Job::$category" />
         </div>
 
-        <div>4</div>
 
       </div>
       <button class="w-full">Filter</button>
