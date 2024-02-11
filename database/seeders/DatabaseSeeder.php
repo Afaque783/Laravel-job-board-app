5b2b037b819f981e727aafd68e9f4890e86a96fb
Afaque783 <ahmed.afaque783@gmail.com>
2024-02-11 21:44:24 +0530

    Application For jobs:Model, Factory, Relations & Migrations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -38,6 +38,18 @@ class DatabaseSeeder extends Seeder
             ]);
         }
 
+        foreach($users as $user){
+            $jobs = \App\Models\Job::inRandomOrder()->take(rand(0,4))->get();
+
+            foreach($jobs as $job){
+                \App\Models\JobApplication::factory()->create([
+                    'job_id' => $job->id,
+                    'user_id' => $user->id
+                ]);
+            }
+
+        }
+
         // \App\Models\Job::factory(100)->create();
         // \App\Models\User::factory(10)->create();
 
